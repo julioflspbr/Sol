af32752cc7493438cca187d1f89313a8d5e2809f
Júlio César Flores <julioflspbr@gmail.com>
2022-05-26 17:05:38 +0200

    Initial commit

---
+++ Sol/SolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolApp.swift
+//  Sol
+//
+//  Created by Júlio César Flores on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
